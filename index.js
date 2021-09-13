19a2915e3f755cf011b3949537702026cbb9a971
thandazilegcisa <gcisatahn@gmail.com>
2021-09-13 10:25:25 +0200

    Device Debugging

--- index.js
+++ index.js
@@ -1,34 +1,14 @@
-var ball = document.querySelector('.ball');
-var garden = document.querySelector('.garden');
-var output = document.querySelector('.output');
+if (window.DeviceOrientationEvent) {
+    console.log("Device Orientation is supported");
 
-var maxX = garden.clientWidth - ball.clientWidth;
-var maxY = garden.clientHeight - ball.clientHeight;
-
-window.ondeviceorientation = function handleOrientation(event) {
-    var x = event.beta;  // In degree in the range [-180,180)
-    var y = event.gamma; // In degree in the range [-90,90)
-
-    output.textContent = `beta : ${x}\n`;
-    output.textContent += `gamma: ${y}\n`;
-
-    // Because we don't want to have the device upside down
-    // We constrain the x value to the range [-90,90]
-    if (x > 90) { x = 90 };
-    if (x < -90) { x = -90 };
-
-    // To make computation easier we shift the range of
-    // x and y to [0,180]
-    x += 90;
-    y += 90;
-
-    // 10 is half the size of the ball
-    // It center the positioning point to the center of the ball
-    ball.style.top = (maxY * y / 180 - 10) + "px";
-    ball.style.left = (maxX * x / 180 - 10) + "px";
-
-    console.log(x);
-    console.log(y);
+    window.addEventListener('deviceorientation', (eventData) => {
+        this.setState({
+            alpha: eventData.alpha,
+            beta: eventData.beta,
+            gamma: eventData.gamma
+        });
+    }, false);
+} else {
+    console.log("Device Orientation is not supported");
 }
-
 
