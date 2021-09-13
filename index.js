5a2b27d5ba00c56d33d08b4e1c075d50c599f954
thandazilegcisa <gcisatahn@gmail.com>
2021-09-13 10:45:24 +0200

    Device Debugging

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-if (window.DeviceOrientationEvent) {
-    console.log("Device Orientation is supported");
-
-    window.addEventListener('deviceorientation', (eventData) => {
-        this.setState({
-            alpha: eventData.alpha,
-            beta: eventData.beta,
-            gamma: eventData.gamma
-        });
-    }, false);
-} else {
-    console.log("Device Orientation is not supported");
-}
+window.addEventListener('deviceorientation', function (event) {
+    console.log(event.alpha + ':' + event.beta + ':' + event.gamma);
+});
 
